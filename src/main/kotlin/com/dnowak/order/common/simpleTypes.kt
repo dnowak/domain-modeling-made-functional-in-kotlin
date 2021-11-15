eeda939fa3debff4eab56220f28907c9eead0f2b
Dariusz Nowak <darek.a.nowak@gmail.com>
2021-11-15 21:38:34 +0100

    Convert simple types with validation

---
+++ src/main/kotlin/com/dnowak/order/common/simpleTypes.kt
@@ -0,0 +1,181 @@
+package com.dnowak.order.common
+
+import arrow.core.*
+import java.math.BigDecimal
+
+/// Constrained to be 50 chars or less, not null
+//type String50 = private String50 of string
+
+class String50 internal constructor(val value: String)
+
+/// An email address
+//type EmailAddress = private EmailAddress of string
+class EmailAddress private constructor(val value: String) {
+    companion object {
+        fun validate(email: String): ValidatedNel<ValidationError, EmailAddress> =
+            validateRegExp(".+@.+", email).bimap(::nonEmptyListOf, ::EmailAddress)
+    }
+}
+
+/// A zip code
+//type ZipCode = private ZipCode of string
+class ZipCode private constructor(val value: String) {
+    companion object {
+        fun validate(zip: String): ValidatedNel<ValidationError, ZipCode> =
+            validateRegExp("\\d{5}", zip).bimap(::nonEmptyListOf, ::ZipCode)
+    }
+}
+
+/// An Id for Orders. Constrained to be a non-empty string < 10 chars
+//type OrderId = private OrderId of string
+class OrderId private constructor(val value: String) {
+    companion object {
+        fun validate(id: String): ValidatedNel<ValidationError, OrderId> =
+            validateStringLength(1, 10, id).bimap(::nonEmptyListOf, ::OrderId)
+    }
+}
+
+/// An Id for OrderLines. Constrained to be a non-empty string < 10 chars
+//type OrderLineId = private OrderLineId of string
+class OrderLineId private constructor(val value: String) {
+    companion object {
+        fun validate(id: String): ValidatedNel<ValidationError, OrderLineId> =
+            validateStringLength(1, 10, id).bimap(::nonEmptyListOf, ::OrderLineId)
+    }
+}
+
+/// The codes for Widgets start with a "W" and then four digits
+//type WidgetCode = private WidgetCode of string
+class WidgetCode private constructor(val value: String) {
+    companion object {
+        fun validate(code: String): ValidatedNel<ValidationError, WidgetCode> =
+            validateRegExp("W\\d{4}", code).bimap(::nonEmptyListOf, ::WidgetCode)
+    }
+}
+
+/// The codes for Gizmos start with a "G" and then three digits.
+//type GizmoCode = private GizmoCode of string
+class GizmoCode private constructor(val value: String) {
+    companion object {
+        fun validate(code: String): ValidatedNel<ValidationError, GizmoCode> =
+            validateRegExp("G\\d{3}", code).bimap(::nonEmptyListOf, ::GizmoCode)
+    }
+}
+
+/// A ProductCode is either a Widget or a Gizmo
+/*
+type ProductCode =
+| Widget of WidgetCode
+| Gizmo of GizmoCode
+ */
+sealed class ProductCode {
+    data class Widget(val value: WidgetCode) : ProductCode()
+    data class Gizmo(val value: GizmoCode) : ProductCode()
+}
+
+/// Constrained to be a integer between 1 and 1000
+//type UnitQuantity = private UnitQuantity of int
+class UnitQuantity private constructor(val value: Int) {
+    companion object {
+        fun validate(quantity: Int): ValidatedNel<ValidationError, UnitQuantity> =
+            validateIntInRange(1, 1000, quantity).bimap(::nonEmptyListOf, ::UnitQuantity)
+    }
+}
+
+/// Constrained to be a decimal between 0.05 and 100.00
+//type KilogramQuantity = private KilogramQuantity of decimal
+class KilogramQuantity internal constructor(val value: BigDecimal) {
+    companion object {
+        private val min = BigDecimal("0.05")
+        private val max = BigDecimal("100.00")
+
+        fun validate(quantity: BigDecimal): ValidatedNel<ValidationError, KilogramQuantity> {
+            return validateBigDecimalInRange(min, max, quantity)
+                .bimap(::nonEmptyListOf, ::KilogramQuantity)
+        }
+    }
+}
+
+/// A Quantity is either a Unit or a Kilogram
+/*
+type OrderQuantity =
+| Unit of UnitQuantity
+| Kilogram of KilogramQuantity
+ */
+sealed class OrderQuantity {
+    data class Unit(val value: UnitQuantity) : OrderQuantity()
+    data class Kilogram(val value: KilogramQuantity) : OrderQuantity()
+}
+
+/// Constrained to be a decimal between 0.0 and 1000.00
+//type Price = private Price of decimal
+class Price private constructor(val value: BigDecimal) {
+    companion object {
+        private val min = BigDecimal("0.00")
+        private val max = BigDecimal("1000.00")
+
+        fun validate(quantity: BigDecimal): ValidatedNel<ValidationError, Price> {
+            return validateBigDecimalInRange(min, max, quantity)
+                .bimap(::nonEmptyListOf, ::Price)
+        }
+    }
+}
+
+/// Constrained to be a decimal between 0.0 and 10000.00
+//type BillingAmount = private BillingAmount of decimal
+class BillingAmount internal constructor(val value: BigDecimal) {
+    companion object {
+        private val min = BigDecimal("0.00")
+        private val max = BigDecimal("10000.00")
+
+        fun validate(quantity: BigDecimal): ValidatedNel<ValidationError, BillingAmount> {
+            return validateBigDecimalInRange(min, max, quantity)
+                .bimap(::nonEmptyListOf, ::BillingAmount)
+        }
+    }
+}
+
+/// Represents a PDF attachment
+/*
+type PdfAttachment = {
+    Name : string
+    Bytes: byte[]
+}
+ */
+class PdfAttachment(val name: String, val bytes: Array<Byte>)
+
+data class ValidationError(val message: String)
+
+fun validateRegExp(pattern: String, value: String): Validated<ValidationError, String> =
+    if (Regex(pattern).matches(value)) {
+        Valid(value)
+    } else {
+        Invalid(ValidationError("'$value' must match the pattern '$pattern'"))
+    }
+
+fun validateStringLength(min: Int, max: Int, value: String): Validated<ValidationError, String> {
+    val length = value.length
+    return if ((length >= min) && (length <= max)) {
+        Valid(value)
+    } else {
+        Invalid(ValidationError("The length of <$value> should be between <$min> and <$max>"))
+    }
+}
+
+fun validateIntInRange(min: Int, max: Int, value: Int): Validated<ValidationError, Int> =
+    if ((value >= min) && (value <= max)) {
+        Valid(value)
+    } else {
+        Invalid(ValidationError("The <$value> should be between <$min> and <$max>"))
+    }
+
+fun validateBigDecimalInRange(
+    min: BigDecimal,
+    max: BigDecimal,
+    value: BigDecimal
+): Validated<ValidationError, BigDecimal> =
+    if ((value >= min) && (value <= max)) {
+        Valid(value)
+    } else {
+        Invalid(ValidationError("The <$value> should be between <$min> and <$max>"))
+    }
---
+++ src/test/kotlin/com/dnowak/order/common/SimpleTypesTest.kt
@@ -0,0 +1,142 @@
+package com.dnowak.order.common
+
+import io.kotest.assertions.arrow.core.shouldBeInvalid
+import io.kotest.assertions.arrow.core.shouldBeValid
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldExist
+import io.kotest.matchers.shouldBe
+import java.math.BigDecimal
+
+internal class SimpleTypesTest : DescribeSpec({
+    describe("EmailAddress.validate") {
+        listOf("me@company.com", "john@google.com").forEach { email ->
+            it("accepts <$email>") {
+                EmailAddress.validate(email).shouldBeValid().value shouldBe email
+            }
+        }
+        listOf("", " ", "@", "me@", "@google.com").forEach { email ->
+            it("rejects <$email>") {
+                EmailAddress.validate(email)
+                    .shouldBeInvalid().all.shouldExist { error -> error.message.contains(email) }
+            }
+        }
+    }
+    describe("ZipCode.validate") {
+        listOf("12345", "11223", "77766").forEach { zip ->
+            it("accepts <$zip>") {
+                ZipCode.validate(zip).shouldBeValid().value shouldBe zip
+            }
+        }
+        listOf("", " ", "1", "12", "123", "1234", "abcde", "ABCDE").forEach { zip ->
+            it("rejects <$zip>") {
+                ZipCode.validate(zip).shouldBeInvalid().all.shouldExist { error -> error.message.contains(zip) }
+            }
+        }
+    }
+    describe("OrderId.validate") {
+        listOf("1", "A", "1234567890", "ABCDEFGHIJ").forEach { id ->
+            it("accepts <$id>") {
+                OrderId.validate(id).shouldBeValid().value shouldBe id
+            }
+        }
+        listOf("", "12345678901", "ABCDEFGHIJK").forEach { id ->
+            it("rejects <$id>") {
+                OrderId.validate(id).shouldBeInvalid().all.shouldExist { error -> error.message.contains(id) }
+            }
+        }
+    }
+    describe("OrderLineId.validate") {
+        listOf("1", "A", "1234567890", "ABCDEFGHIJ").forEach { id ->
+            it("accepts <$id>") {
+                OrderLineId.validate(id).shouldBeValid().value shouldBe id
+            }
+        }
+        listOf("", "12345678901", "ABCDEFGHIJK").forEach { id ->
+            it("rejects <$id>") {
+                OrderLineId.validate(id).shouldBeInvalid().all.shouldExist { error -> error.message.contains(id) }
+            }
+        }
+    }
+    describe("WidgetCode.validate") {
+        listOf("W1234", "W4444", "W2222").forEach { value ->
+            it("accepts <$value>") {
+                WidgetCode.validate(value).shouldBeValid().value shouldBe value
+            }
+        }
+        listOf("", "W", "W1", "W123", "Z1234", "G1234").forEach { value ->
+            it("rejects <$value>") {
+                WidgetCode.validate(value).shouldBeInvalid().all.shouldExist { error -> error.message.contains(value) }
+            }
+        }
+    }
+    describe("GizmoCode.validate") {
+        listOf("G123", "G444", "G222").forEach { value ->
+            it("accepts <$value>") {
+                GizmoCode.validate(value).shouldBeValid().value shouldBe value
+            }
+        }
+        listOf("", "G", "G1", "W123", "G1234", "G12345").forEach { value ->
+            it("rejects <$value>") {
+                GizmoCode.validate(value).shouldBeInvalid().all.shouldExist { error -> error.message.contains(value) }
+            }
+        }
+    }
+    describe("UnitQuantity.validate") {
+        listOf(1, 10, 500, 999, 1000).forEach { value ->
+            it("accepts <$value>") {
+                UnitQuantity.validate(value).shouldBeValid().value shouldBe value
+            }
+        }
+        listOf(-10000, -100, -1, 0, 1001, 10000).forEach { value ->
+            it("rejects <$value>") {
+                UnitQuantity.validate(value)
+                    .shouldBeInvalid().all.shouldExist { error -> error.message.contains(value.toString()) }
+            }
+        }
+    }
+    describe("KilogramQuantity.validate") {
+        listOf("0.05", "0.10", "0.5", "10.50", "99.99", "100.00").forEach { value ->
+            it("accepts <$value>") {
+                val kg = BigDecimal(value)
+                KilogramQuantity.validate(kg).shouldBeValid().value shouldBe kg
+            }
+        }
+        listOf("0.00", "0.01", "0.04", "100.01", "101.00", "1000.00").forEach { value ->
+            it("rejects <$value>") {
+                val kg = BigDecimal(value)
+                KilogramQuantity.validate(kg)
+                    .shouldBeInvalid().all.shouldExist { error -> error.message.contains(value) }
+            }
+        }
+    }
+    describe("Price.validate") {
+        listOf("0.00", "0.01", "0.05", "0.10", "0.5", "10.50", "999.99", "1000.00").forEach { value ->
+            it("accepts <$value>") {
+                val price = BigDecimal(value)
+                Price.validate(price).shouldBeValid().value shouldBe price
+            }
+        }
+        listOf("-1000.00", "-0.01", "1000.01", "10000.00").forEach { value ->
+            it("rejects <$value>") {
+                val price = BigDecimal(value)
+                Price.validate(price)
+                    .shouldBeInvalid().all.shouldExist { error -> error.message.contains(value) }
+            }
+        }
+    }
+    describe("BillingAmount.validate") {
+        listOf("0.00", "0.01", "0.05", "0.10", "0.5", "10.50", "9999.99", "10000.00").forEach { value ->
+            it("accepts <$value>") {
+                val amount = BigDecimal(value)
+                BillingAmount.validate(amount).shouldBeValid().value shouldBe amount
+            }
+        }
+        listOf("-1000.00", "-0.01", "10000.01", "100000.00").forEach { value ->
+            it("rejects <$value>") {
+                val amount = BigDecimal(value)
+                BillingAmount.validate(amount)
+                    .shouldBeInvalid().all.shouldExist { error -> error.message.contains(value) }
+            }
+        }
+    }
+})
